11455ea18527277240cf2b22d71a77c21ec30310
Cix <gabrielviniciodacosta@gmail.com>
2025-06-28 11:38:17 -0300

    first commit

---
+++ churras/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Prato
+
+# Create your views here.
+def index(request):
+    lista_pratos = Prato.objects.all()
+    return render(request, 'index.html', {'lista_pratos': lista_pratos})
+
+def churrasco(request, id):
+    prato = get_object_or_404(Prato, id=id)
+    return render(request, 'churrasco.html', {'prato': prato})
+
